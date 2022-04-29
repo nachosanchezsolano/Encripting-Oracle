39599ffd0cdcf7d603ebb00b6d20d72a73229210
nachosanchezsolano <77471637+nachosanchezsolano@users.noreply.github.com>
2022-04-28 23:36:18 -0300

    the scrypt its ok

--- functions/encript.js
+++ functions/encript.js
@@ -1,60 +1,89 @@
 
-
-
-
     /////encrypting Function///////
 function scryptFunction(){
 
-    /////// declared variables ///////
-    
-                //text variables//
-    let textImput=document.getElementById("textImput").value;
-    let textEncrypt= Array.from(textImput);
-                //img variables//
+            /////// declared variables ///////
+            
+                        //text variables//
+            let textImput=document.getElementById("textImput").value;
+            let textEncrypt= Array.from(textImput);
 
-                if 
-                let imgHide = document.getElementById("dontFoundMassageImg").style;
+            ///// replace characters///////
+            for(let i=0; i<=textEncrypt.length; i++){
+                        switch (textEncrypt[i]) {
+                                            case "a" : textEncrypt[i] = "ai";
+                                                            break;
+                                            case "e" : textEncrypt[i] = "enter";
+                                                            break;
+                                            case "i" : textEncrypt[i] = "imes";
+                                                            break;
+                                            case "o": textEncrypt[i] = "ober";
+                                                            break;
+                                            case "u": textEncrypt[i] = "ufat";
+                                                            break;
+                                            default : break;
+                                            }
+        }
 
+        textEncrypt = textEncrypt.toString();
+        let re = /,/gi;
+        textEncrypt = textEncrypt.replace(re,"");
 
+        console.log(textEncrypt);
 
-    ///// replace characters///////
-    for(let i=0; i<=textEncrypt.length; i++){
-                switch (textEncrypt[i]) {
-                                    case "a" : textEncrypt[i] = "ai";
-                                                    break;
-                                    case "e" : textEncrypt[i] = "enter";
-                                                    break;
-                                    case "i" : textEncrypt[i] = "imes";
-                                                    break;
-                                    case "o": textEncrypt[i] = "ober";
-                                                    break;
-                                    case "u": textEncrypt[i] = "ufat";
-                                                    break;
-                                    default : break;
-                                    }
-}
-
-textEncrypt = textEncrypt.toString();
-let re = /,/gi;
-textEncrypt = textEncrypt.replace(re,"");
-
-console.log(textEncrypt);
-
-/////import de text exctypted into de document//////
-imgHide.display = "none"
-
-document.getElementById("textoptput").innerHTML = textEncrypt
+            /////import de text exctypted into de document//////
 
 
-}
+        let imgAsk
+
+        if (imgAsk == false ){
+
+            let imgHide = document.getElementById("dontFoundMassageImg").style;
+
+            imgHide.display = "none"
+
+            imgAsk==true
+
+            }
+
+            document.getElementById("textoutput").innerHTML = textEncrypt
+
+        }
 
 
 
 
     ///////// decrypting funtion//////
 function decryptFunction(){
-    return alert(mensaje);
+
+                    //text variables//
+                    let  textDecrypt=document.getElementById("textImput").value;
+
+                    ///// replace characters///////
+
+         textDecrypt =  textDecrypt.replace(/ai/g,"a");
+         textDecrypt =  textDecrypt.replace(/enter/g,"e");
+         textDecrypt =  textDecrypt.replace(/imes/g,"i");
+         textDecrypt =  textDecrypt.replace(/ober/g,"o");
+         textDecrypt =  textDecrypt.replace(/ufat/g,"u");
+
+        console.log(textDecrypt);
+
+            /////import de text exctypted into de document//////
 
 
+        let imgAsk
 
-}
+        if (imgAsk == false ){
+
+            let imgHide = document.getElementById("dontFoundMassageImg").style;
+
+            imgHide.display = "none";
+
+            imgAsk==true;
+
+            }
+
+            document.getElementById("textoutput").innerHTML = textDecrypt;
+
+        }
